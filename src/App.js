4840a6f11e24d915a784200e64caa316fd6c95e6
Tanisha <tanisha031199@gmail.com>
2020-04-30 20:18:16 +0530

    added emotion

--- src/App.js
+++ src/App.js
@@ -2,18 +2,27 @@ import React from 'react'
 import TaskList from './components/TaskList'
 import TaskForm from './components/TaskForm'
 import TaskListContextProvider from './context/TaskContext'
+import styled from '@emotion/styled'
+
+const Main= styled.div`
+ width:40vw;
+ background-color:pink;
+ padding:10%;
+ margin: 20vh auto;
+ text-align:center;
+`;
 
 export default function App() {
     return (
         <TaskListContextProvider>
-        <div className="container">
-        <div className="app-wrapper">
-        <div className="main">
+        {/* <div className="container"> */}
+        {/* <div className="app-wrapper"> */}
+        <Main>
             <TaskForm/>
             <TaskList/>
-        </div>
-        </div>
-        </div>
+        </Main>
+        {/* </div> */}
+        {/* </div> */}
         </TaskListContextProvider>
     )
 }
